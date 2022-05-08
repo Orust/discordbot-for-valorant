6d1504e1350415aef758ea8c438814bf5c89ba2c
Tatsuro <orust4t@gmail.com>
2022-05-08 12:02:55 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, Intents } = require('discord.js');
+
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", async () => {
+    const data = [{
+        name: "ping",
+        description: "Replies with Pong!",
+    }];
+    await client.application.commands.set(data);
+    console.log("Ready!");
+});
+
+client.login()
+  .catch(console.error)
