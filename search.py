a86a7e9a7da097ee5a2b2080d95a832be2f81c8a
Tatsuro <orust4t@gmail.com>
2022-05-10 11:14:11 +0900

    add debug text

--- search.py
+++ search.py
@@ -1,9 +1,11 @@
 import sys
 import pandas as pd
 
+import requests
 from bs4 import BeautifulSoup
+import urllib.parse as parse
 
 
 data = sys.stdin.readline()
 
-print(data + ' in python.')
+print(data + ' in python. test import and add two file')
