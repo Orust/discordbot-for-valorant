421a4f01212842bd7d94aeb74e56d1f8c125e659
Tatsuro <orust4t@gmail.com>
2022-05-16 09:53:41 +0900

    delete async

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.once("ready", async () => {
     console.log("Ready!");
 });
 
-client.on("interactionCreate", async (interaction) => {
+client.on("interactionCreate", (interaction) => {
     if (!interaction.isCommand()) {
         return;
     }
