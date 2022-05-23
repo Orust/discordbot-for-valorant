25fd90416da8a5b0c4549168efaa835ba64133d8
Tatsuro <orust4t@gmail.com>
2022-05-23 09:57:49 +0900

    import requests

--- search.py
+++ search.py
@@ -1,25 +1,18 @@
 import sys, pprint
 
-# import os
 # import firebase_admin
 # from firebase_admin import credentials
 # from firebase_admin import db
 
 
 
-# cred = credentials.Certificate("./discord-valorant-matching-firebase-adminsdk-9gsja-2457bb54cd.json")
-# firebase_admin.initialize_app(cred)
-
 # import pandas as pd
 
-# import requests
+import requests
 from bs4 import BeautifulSoup
-# import urllib.parse as parse
 
 
 data = sys.stdin.readline()
-
-# dir = os.getcwd()
 
 pprint.pprint(sys.path)
 pprint.pprint(sys.platform) # linux
