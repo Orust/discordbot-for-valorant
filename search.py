a8812669b2a3579c442c252e5c606ac283c9fc0e
Tatsuro <orust4t@gmail.com>
2022-05-23 18:15:29 +0900

    get stats

--- search.py
+++ search.py
@@ -12,9 +12,38 @@ import urllib.parse as parse
 
 data = sys.stdin.readline()
 
-pprint.pprint(sys.path)
-pprint.pprint(sys.platform) # linux
-print(data + ' in python test.')
+# pprint.pprint(sys.path)
+# pprint.pprint(sys.platform) # linux
+# print(data + ' in python test.')
 
-# input_name = data
+input_name = data
 
+user_name = parse.quote(input_name)
+load_url = "https://tracker.gg/valorant/profile/riot/" + user_name + "/agents?playlist=competitive&season=all"
+load_url2 = "https://tracker.gg/valorant/profile/riot/" + user_name + "/overview?playlist=competitive&season=all"
+html = urllib.request.urlopen(load_url)
+html2 = urllib.request.urlopen(load_url2)
+soup = BeautifulSoup(html.content, "html.parser")
+soup_overview = BeautifulSoup(html2.content, "html.parser")
+
+stat_class = soup.select('.agent__stat')
+name_class = soup.select('.agent__name-name')
+rank_class = soup_overview.select('.value')
+matches = []
+names = []
+print(name_class)
+print(rank_class)
+rank = rank_class[0].get_text().strip()
+
+
+for i in range(len(name_class)):
+    names.append(name_class[i].get_text())
+
+for i in range(len(stat_class)):
+    if i % 6 == 0:
+        matches.append(int(stat_class[i].get_text()))
+
+# print(names)
+# print(matches)
+print(rank)
+# print(soup.find_all(class_="agent__stat"))
