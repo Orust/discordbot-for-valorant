9aeadd9eda6fa64bdb2b5143e03e902d51563f20
Tatsuro <orust4t@gmail.com>
2022-05-31 09:27:04 +0900

    cout import numpy

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 # import pandas as pd
-import numpy as np
+# import numpy as np
 
 from bs4 import BeautifulSoup
 import requests
