8bdfaafbfe53ac249cab3c335c0d579d97a7dd8e
Tatsuro <orust4t@gmail.com>
2022-07-05 04:00:05 +0900

    channel send like sample embeds

--- index.js
+++ index.js
@@ -661,13 +661,15 @@ client.on("interactionCreate", async (interaction) => {
             ephemeral: true
         });
         */
-        /*
+        
         const attachment = new MessageAttachment(outputBuffer, "./test.png");
+        /*
         await interaction.reply("image", {
             embeds: [new MessageEmbed({ title: "test", files: ["test.png"]})]
         });
         */
         
+        interaction.channel.send({ embeds: [attachment] });
         await interaction.reply("image", { files: ["./test.png"] });
     }
 });
